55c72c8439d9afb28795141cc5f1f75283b8ab52
Jeeva <azhagusundaram.t@zohocorp.com>
2025-03-06 17:38:40 +0530

    Payment listing and row action implemented

--- app/bill-entries/[id]/payments/page.tsx
+++ app/bill-entries/[id]/payments/page.tsx
@@ -3,9 +3,34 @@ import { prisma } from "@/prisma/client";
 import React from "react";
 import { columns } from "./columns";
 import PaymentsListPage from "../components/payments-list-page";
+import { Box, Text } from "@radix-ui/themes";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PlusSquare } from "lucide-react";
 
 const PaymentsPage = async ({ params }: { params: { id: string } }) => {
-  return <PaymentsListPage billEntryId={params.id} />;
+  return (
+    <Box>
+      <Box className="flex justify-between items-center">
+        <Box>
+          <Text>
+            <strong>Payments</strong>
+          </Text>
+        </Box>
+        <Box>
+          <Button variant="outline">
+            <Link href={`/bill-entries/${params.id}/payments/new`}>
+              <Box className="flex items-center space-x-2">
+                <PlusSquare />
+                <span>Add Payments</span>
+              </Box>
+            </Link>
+          </Button>
+        </Box>
+      </Box>
+      <PaymentsListPage billEntryId={params.id} />
+    </Box>
+  );
 };
 
 export default PaymentsPage;
